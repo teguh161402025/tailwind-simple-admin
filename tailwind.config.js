b9da38ec9f69b86ab1f02261ad3e77fc4b727691
Teguh161402025 <teguhvmc@gmail.com>
2023-11-01 19:35:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f7f7f7',
+        'secondary': '#dddddd',
+        'fontlight': '#18191A',
+        'banner': '#AC94F4'
+      },
+    },
+
+  },
+  plugins: [],
+}
